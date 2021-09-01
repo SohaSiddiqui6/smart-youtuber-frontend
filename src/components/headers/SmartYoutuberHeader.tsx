460c7960ed6931e9fa906797797ce6ac5c650413
Dayem Siddiqui <dayemsiddiqui@gmail.com>
2021-09-01 20:42:09 +0200

    updated the title

--- src/components/headers/SmartYoutuberHeader.tsx
+++ src/components/headers/SmartYoutuberHeader.tsx
@@ -66,7 +66,7 @@ const SmartYoutuberHeader: React.FC<any> = ({ logoLink, links, className }) => {
 
   const defaultLogoLink = (
     <LogoLink tw="text-purple-700" href="/">
-      Smart Youtuber
+      Unigow Platform
     </LogoLink>
   );
 
