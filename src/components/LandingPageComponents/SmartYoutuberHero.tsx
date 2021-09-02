d68a93704d84837d4e6f7f949d7c244dd2faf629
SohaSiddiqui6 <siddiquisoha6@gmail.com>
2021-09-02 14:44:54 +0500

    new changes

--- src/components/LandingPageComponents/SmartYoutuberHero.tsx
+++ src/components/LandingPageComponents/SmartYoutuberHero.tsx
@@ -12,7 +12,7 @@ import { Redirect } from "react-router-dom";
 
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
-const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left`;
+const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left top-0`;
 const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
 
 const Heading = tw.h1`font-bold text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-gray-900 leading-tight`;
